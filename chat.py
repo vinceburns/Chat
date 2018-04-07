cc1ef7b2c685eb8499130262b216d19e3ae20e25
Vince Burns <vburns@xirgotech.com>
2018-04-07 18:00:36 -0500

    create a socket in main to pass to our threads

--- chat.py
+++ chat.py
@@ -28,7 +28,20 @@ class ListeningThread(threading.Thread,our_socket):
         
         
 def main():
-    thread_pool =[KeyboardThread,SendingThread,ListeningThread]
+    UDP_PORT = raw_input("Please enter port that you have forwarding set up on: ")
+        try:
+        our_sock = socket.socket(socket.AF_INET,
+                         socket.SOCK_DGRAM)
+        #taking it as raw should implement as an argument.
+        
+        our_sock.bind(("",UDP_PORT))
+        #sock.bind takes a Tuple of (Host,Port), "" takes all available hosts (for us i think its just Internal/external IP.)
+        print "Socket open! Listening..."
+    except:
+        print "Couldn't open socket. Probably have a different server running on that port. Program Exit."
+        return 0
+    #we finna pass this socket around to send and receive data on. We should eventually pass a log file around/ Dictionary of addresses joining our chat???
+    thread_pool =[KeyboardThread,SendingThread(our_sock),ListeningThread(our_sock)]    
     for t in thread_pool:
         t.start()
     while True:
